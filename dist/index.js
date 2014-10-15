8ccfcc476b7f1796166912b3f318a5d30f49cc4a
mattstyles <matt@veryfizzyjelly.com>
2014-10-15 21:37:44 +0100

    1.0.0

--- dist/index.js
+++ dist/index.js
@@ -1,8 +1,23 @@
 "format register";
 
-System.register("index", [], false, function(__require, __exports, __module) {
-  System.get("@@global-helpers").prepareGlobal(__module.id, []);
-  (function() {  
-      
-  }).call(System.global);  return System.get("@@global-helpers").retrieveGlobal(__module.id, false);
+System.register("index", [], function($__export) {
+  "use strict";
+  var __moduleName = "index";
+  return {
+    setters: [],
+    execute: function() {
+      $__export('default', (function() {
+        var screenBaseModule = function screenBaseModule() {
+          var name = arguments[0] !== (void 0) ? arguments[0] : 'BaseModule';
+          this.name = name;
+        };
+        return ($traceurRuntime.createClass)(screenBaseModule, {
+          init: function() {},
+          expose: function() {
+            return {};
+          }
+        }, {});
+      }()));
+    }
+  };
 });
