906e4c32b406c5c8520f8e631126b7bce03f2373
mattstyles <matt@veryfizzyjelly.com>
2014-10-15 21:06:12 +0100

    initial commit

---
+++ dist/index.js
@@ -0,0 +1,8 @@
+"format register";
+
+System.register("index", [], false, function(__require, __exports, __module) {
+  System.get("@@global-helpers").prepareGlobal(__module.id, []);
+  (function() {  
+      
+  }).call(System.global);  return System.get("@@global-helpers").retrieveGlobal(__module.id, false);
+});
