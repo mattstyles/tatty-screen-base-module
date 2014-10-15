3036ed754ecd47208c5db8831d47c79ff401089c
mattstyles <matt@veryfizzyjelly.com>
2014-10-15 21:35:05 +0100

    #add - skeleton

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 
+export default class screenBaseModule {
+
+    /**
+     * @constructs
+     */
+    constructor( name='BaseModule') {
+        this.name = name;
+    }
+
+    /**
+     * Initialise module
+     */
+    init() {
+
+    }
+
+    /**
+     * Exposed API
+     */
+    expose() {
+        return {
+
+        }
+    }
+}
